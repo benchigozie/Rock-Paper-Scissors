fde3ee75d86c6ea01a59efd672fde53cbb2e0fff
Ben <bennchigozie@gmail.com>
2022-11-16 02:10:39 +0100

    Index.html

--- main.js
+++ main.js
@@ -1,63 +1,70 @@
 let choices = ["rock", "paper", "scissors"];
+let pWins = 0;
+let cWins = 0;
 function getComputerChoice() {
+  //update the dom with computer selection
   let choice = choices[Math.floor(Math.random() * 3)];
   return choice;
-}
-function getPlayerChoice(){
+};
+function reset(){
+  cWins = 0;
+  pWins = 0;
+  document.querySelector(".reset button").style.display = "none";
+  document.querySelector(".player-score").textContent = `You: ${pWins}`;
+  document.querySelector(".computer-score").textContent = `Computer: ${cWins}`;
+  document.querySelector(".displays").textContent = "";
+};
+function playRound(playerSelection){
+  if (pWins >= 5 || cWins >= 5){
+    
+    endRound(pWins, cWins);
+    return;
+  };
+  let computerSelection = getComputerChoice();
   
-}
-
-function winnerCheck(playerSelection, computerSelection) {
-  if(playerSelection == computerSelection){
-    return "tie";
+  if(winnerCheck(playerSelection, computerSelection) == "computer"){
+    cWins++;
   }
-  else if (
+  if(winnerCheck(playerSelection, computerSelection) == "player"){
+    pWins++;
+  }
+  document.querySelector(".player-score").textContent = `You: ${pWins}`;
+  document.querySelector(".computer-score").textContent = `Computer: ${cWins}`;
+}
+function endRound(pWins, cWins){
+  document.querySelector(".reset button").style.display = "block";
+  if(pWins > cWins){
+    document.querySelector(".displays").style.fontSize = "3 rem";
+    document.querySelector(".displays").textContent = `Congratulations, You won the Game`;
+  }
+  else{
+    document.querySelector(".displays").style.fontSize = "3 rem";
+    document.querySelector(".displays").textContent = `Boo, Computer won the game`;
+  };
+  let resetButton = document.querySelector(".reset");
+  resetButton.addEventListener(("click"), () => {reset()});
+}
+function winnerCheck(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    document.querySelector(".displays").textContent = "You draw, Its a tie";
+    return "tie";
+  } else if (
     (playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "scissors" && computerSelection == "paper") ||
     (playerSelection == "paper" && computerSelection == "rock")
   ) {
-    return "player";
-  }
-  else {
+    document.querySelector(".displays").textContent = `You won, ${playerSelection} beats ${computerSelection}`;
+    return "player"
+  } else {
+    document.querySelector(".displays").textContent = `Computer won, ${computerSelection} beats ${playerSelection}`;
     return "computer";
   }
-}
-function gameRound(playerSelection, computerSelection){
-    let result = winnerCheck(playerSelection, computerSelection);
-    if (result == "tie"){
-        return "It's a tie";
-    }
-    else if(result == "player"){
-        return `You win! ${playerSelection} beats ${computerSelection}`;
-    }
-    else {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
-    }
-}
-function game(){
-  let playerScore = 0;
-  let computerScore = 0;
-for (let a = 0; a < 5; a++){
-    const computerSelection = getComputerChoice();
-    const playerSelection = getPlayerChoice();
-    console.log(`you chose ${playerSelection}`);
-    console.log(`computer chose ${computerSelection}`);
-    console.log(gameRound(playerSelection, computerSelection));
-    console.log("_______________");
-  if(winnerCheck(playerSelection, computerSelection) == "player"){
-      playerScore++;
-    }
-    else if(winnerCheck(playerSelection, computerSelection) == "computer"){
-      computerScore++;
-    }
-}
-console.log(computerScore + ": " + playerScore);
-if(computerScore > playerScore){
-  console.log("Computer won the game");
-}
-else if (playerScore > computerScore){
-  console.log("You won the round. Beast.");
-}
+};
 
-}
+function game() {
+  let imgs = document.querySelectorAll("img");
+  imgs.forEach((image) => {image.addEventListener(('click'), () => {playRound(image.id);
+  })
+});
+};
 game();
